c1d272f715804240fef9f07350585047280d8ef0
Katie Byers <lobsterkatie@gmail.com>
2015-11-03 18:25:13 -0800

    IDK - a bunch of stuff happened; I should commit more often (I think: got registration and login modals to cross-link; tried and failed to left-align cross-linking messages; got login modal to work at all;added route stubs for showing the dashboard and logging in; maybe other things?)

--- server.py
+++ server.py
@@ -14,13 +14,19 @@ app.secret_key = "shhhhhhhhhhh!!! don't tell!"
 app.jinja_env.undefined = StrictUndefined
 
 
-@app.route('/')
+#################### ROUTES TO SHOW HOMEPAGE AND DASHBOARD ####################
+
+
+@app.route("/")
 def index():
     """Show the homepage"""
 
-    # return "I exist!"
-
     return render_template("home.html")
+
+
+@app.route("/<string: username>")
+def show_dashboard(username):
+    pass
 
 
 #################### LOGIN, LOGOUT, AND REGISTRATION ROUTES ####################
@@ -29,11 +35,16 @@ def index():
 def check_email_uniqueness():
     """Checks the given email against the User table. Returns false if found."""
 
+    #get the username from the form validator
     email = request.args.get("email")
+
+    #count how many users in the table have that username (should be 1 or 0)
     num_matched = (db.session.query(User)
                              .filter(User.email == email)
                              .count())
 
+    #if there's already a user with that email, return false to indicate
+    #that the given email isn't new/unique; return true otherwise
     if num_matched == 1:
         return "false"
     else:
@@ -44,11 +55,16 @@ def check_email_uniqueness():
 def check_username_uniqueness():
     """Checks the given username against the User table. Returns false if found."""
 
+    #get the username from the form validator
     username = request.args.get("username")
+
+    #count how many users in the table have that username (should be 1 or 0)
     num_matched = (db.session.query(User)
                              .filter(User.username == username)
                              .count())
 
+    #if there's already a user with that username, return false to indicate
+    #that the given username isn't new/unique; return true otherwise
     if num_matched == 1:
         return "false"
     else:
@@ -62,6 +78,7 @@ def add_new_user():
        Note that all form data is validated before being submitted, so the
        values here should just work"""
 
+    #get data from the form
     firstname = request.form.get("firstname")
     lastname = request.form.get("lastname")
     gender = request.form.get("gender")
@@ -74,16 +91,31 @@ def add_new_user():
     #TODO implement timezones
     weight = request.form.get("weight")
 
+    #create the new user and add them to the database
     new_user = User(firstname=firstname, lastname=lastname, gender=gender,
                     birthdate=birthdate, zipcode=zipcode, email=email,
                     username=username, password=password, weight=weight)
     db.session.add(new_user)
     db.session.commit()
 
-    #HOW DO I GET IT TO GO TO THE LOGIN MODAL? SAME QUESTION FOR LINK
-    #WHICH NEEDS TO BE ADDED TO REGISTRATION MODAL
+    #pull the user from the database (where they should now be), so that
+    #the user object contains the auto-assigned values as well (which the
+    #new_user object we created above doesn't)
+    added_user = (db.session.query(User)
+                            .filter(User.username == username)
+                            .one())
+
+    #add the user to the session for easy grabbing and to signify thier
+    #logged-in state
+    session["user"] = added_user
+
+    #display the user's dashboard page, in logged-in state
+    return redirect("/" + username)
 
 
+@app.route("/login/<int: user_id>")
+def log_user_in(user_id):
+    pass
 
 
 
